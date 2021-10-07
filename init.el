960772d265c4a1cdd1b99a4e8c3908f082f2fafa
Eason0210 <aqua0210@163.com>
2021-10-07 17:32:27 +0800

    make TAB more smarter with company.

--- init.el
+++ init.el
@@ -231,9 +231,27 @@
   :diminish
   :bind
   (:map company-active-map
-        ("TAB" . company-complete-selection)
-        ("<tab>" . company-complete-selection))
-  :hook (after-init . global-company-mode))
+        ("TAB" . smarter-tab-to-complete)
+        ("<tab>" . smarter-tab-to-complete))
+  :hook (after-init . global-company-mode)
+  :config
+  (defun smarter-tab-to-complete ()
+    "Try to `org-cycle', `yas-expand', and `yas-next-field' at current cursor position.
+If all failed, try to complete the common part with `company-complete-common'"
+    (interactive)
+    (when yas-minor-mode
+      (let ((old-point (point))
+            (old-tick (buffer-chars-modified-tick))
+            (func-list
+             (if (equal major-mode 'org-mode) '(org-cycle yas-expand yas-next-field)
+               '(yas-expand yas-next-field))))
+        (catch 'func-suceed
+          (dolist (func func-list)
+            (ignore-errors (call-interactively func))
+            (unless (and (eq old-point (point))
+                         (eq old-tick (buffer-chars-modified-tick)))
+              (throw 'func-suceed t)))
+          (company-complete-common))))))
 
 
 (use-package consult
