bb643784769ccbd98f1928b1ba51aa88876ca841
Eason0210 <aqua0210@163.com>
2021-10-07 14:10:05 +0800

    add emacs-rime.

--- init.el
+++ init.el
@@ -481,6 +481,46 @@
   (setq auto-save-idle 1.5)
   (auto-save-enable))
 
+;; Rime
+(defconst rime-usr-data-exists-p
+  (file-exists-p "~/emacs-data/rime")
+  "Checking if there is a rime user data.")
+
+(use-package rime
+  :if rime-usr-data-exists-p
+  :ensure t
+  :bind (("C-\\" . toggle-input-method)
+         ("C-`" . rime-send-keybinding))
+  :init
+
+  (setq
+   rime-inline-predicates '(rime-predicate-space-after-cc-p
+                            rime-predicate-current-uppercase-letter-p)
+   rime-translate-keybindings '("C-f" "C-b" "C-n" "C-p" "C-g")
+   rime-inline-ascii-trigger 'shift-r
+   rime-inline-ascii-holder ?a
+   default-input-method "rime"
+   rime-cursor "|"
+   rime-show-candidate nil
+   window-min-height 1
+   rime-user-data-dir "~/emacs-data/rime"
+   rime-title "")
+
+  (when (eq system-type 'windows-nt)
+    (setq rime-share-data-dir
+          "~/scoop/apps/msys2/current/mingw64/share/rime-data"))
+  (when *is-a-mac*
+    (setq rime-librime-root  "~/emacs-data/librime/dist")
+    (setq rime-emacs-module-header-root "~/.nix-profile/include"))
+
+  
+  (defun rime-toggle-show-candidate ()
+    "Use minibuffer for candidate if current is nil."
+    (interactive)
+    (if (equal rime-show-candidate nil)
+        (setq rime-show-candidate 'minibuffer)
+      (setq rime-show-candidate nil))))
+
 
 ;;; Programming languages support
 
