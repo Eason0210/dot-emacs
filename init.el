c99697092f52348f0a196b2ba1b0db93bb4ff87e
Eason0210 <aqua0210@163.com>
2021-10-07 20:04:09 +0800

    add writeroom and only show line number in prog-mode.

--- init.el
+++ init.el
@@ -152,8 +152,9 @@
 ;;; Show line number
 
 (use-package display-line-numbers
+  :hook (prog-mode . display-line-numbers-mode)
   :config
-  (global-display-line-numbers-mode))
+  (setq-default display-line-numbers-width 3))
 
 
 ;;; Minibuffer config
@@ -712,6 +713,51 @@ If all failed, try to complete the common part with `company-complete-common'"
                              docx-file
                              template-file))
       (message "Convert finish: %s" docx-file))))
+
+
+;; Writing mode similar to the famous Writeroom editor for OS X
+(use-package writeroom-mode
+  :ensure t
+  :hook (org-mode . prose-mode)
+  :config
+  (define-minor-mode prose-mode
+    "Set up a buffer for prose editing.
+This enables or modifies a number of settings so that the
+experience of editing prose is a little more like that of a
+typical word processor."
+    :init-value nil :lighter " Prose" :keymap nil
+    (if prose-mode
+        (progn
+          (when (fboundp 'writeroom-mode)
+            (writeroom-mode 1))
+          (setq truncate-lines nil)
+          (setq word-wrap t)
+          (setq word-wrap-by-category t)
+          (setq cursor-type 'bar)
+          (when (eq major-mode 'org)
+            (kill-local-variable 'buffer-face-mode-face))
+          (buffer-face-mode 1)
+          ;;(delete-selection-mode 1)
+          (setq-local blink-cursor-interval 0.6)
+          (setq-local show-trailing-whitespace nil)
+          (setq-local line-spacing 0.2)
+          (setq-local electric-pair-mode nil)
+          ;; (ignore-errors (flyspell-mode 1))
+          (visual-line-mode 1))
+      (kill-local-variable 'truncate-lines)
+      (kill-local-variable 'word-wrap)
+      (kill-local-variable 'word-wrap-by-category)
+      (kill-local-variable 'cursor-type)
+      (kill-local-variable 'blink-cursor-interval)
+      (kill-local-variable 'show-trailing-whitespace)
+      (kill-local-variable 'line-spacing)
+      (kill-local-variable 'electric-pair-mode)
+      (buffer-face-mode -1)
+      ;; (delete-selection-mode -1)
+      ;; (flyspell-mode -1)
+      (visual-line-mode -1)
+      (when (fboundp 'writeroom-mode)
+        (writeroom-mode 0)))))
 
 
 ;; Roam
