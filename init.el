ea1df4ee4410e6533bdf82b8fc50fff31b9b62dd
Eason0210 <aqua0210@163.com>
2021-10-07 17:28:48 +0800

    add yasnippet.

--- init.el
+++ init.el
@@ -201,6 +201,31 @@
   (setq enable-recursive-minibuffers t))
 
 
+;; YASnippet
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :init
+  (use-package yasnippet-snippets :ensure t :after yasnippet)
+  :hook ((prog-mode org-mode) . yas-minor-mode)
+  :bind
+  (:map yas-minor-mode-map ("C-c C-n" . yas-expand-from-trigger-key))
+  (:map yas-keymap
+        (("TAB" . smarter-yas-expand-next-field)
+         ([(tab)] . smarter-yas-expand-next-field)))
+  :config
+  (yas-reload-all)
+  (defun smarter-yas-expand-next-field ()
+    "Try to `yas-expand' then `yas-next-field' at current cursor position."
+    (interactive)
+    (let ((old-point (point))
+          (old-tick (buffer-chars-modified-tick)))
+      (yas-expand)
+      (when (and (eq old-point (point))
+                 (eq old-tick (buffer-chars-modified-tick)))
+        (ignore-errors (yas-next-field))))))
+
+;; Completion with company
 (use-package company
   :ensure t
   :diminish
