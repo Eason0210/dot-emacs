675a4018e2ca6600376048051413fea56c01b9e3
Eason0210 <aqua0210@163.com>
2021-10-09 06:22:25 +0800

    remove this to startup faster.

--- init.el
+++ init.el
@@ -911,19 +911,6 @@ typical word processor."
 
 ;;; Configure default locale
 
-(defun aqua/locale-var-encoding (v)
-  "Return the encoding portion of the locale string V, or nil if missing."
-  (when v
-    (save-match-data
-      (let ((case-fold-search t))
-        (when (string-match "\\.\\([^.]*\\)\\'" v)
-          (intern (downcase (match-string 1 v))))))))
-
-(dolist (varname '("LC_ALL" "LANG" "LC_CTYPE"))
-  (let ((encoding (aqua/locale-var-encoding (getenv varname))))
-    (unless (memq encoding '(nil utf8 utf-8))
-      (message "Warning: non-UTF8 encoding in environment variable %s may cause interop problems with this Emacs configuration." varname))))
-
 (when (fboundp 'set-charset-priority)
   (set-charset-priority 'unicode))
 (prefer-coding-system 'utf-8)
