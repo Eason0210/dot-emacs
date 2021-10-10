56f3ce2f587de34dd9bd9e0b77fd4aa942a1c9de
Eason0210 <aqua0210@163.com>
2021-10-10 19:40:33 +0800

    setup for Web programming.

--- init.el
+++ init.el
@@ -698,6 +698,53 @@ Call a second time to restore the original window configuration."
   (setq-default compilation-scroll-output t))
 
 
+;;; Web configurations
+
+;; SASS and SCSS mode
+;; (use-package sass-mode :defer t)
+(setq-default scss-compile-at-save nil)
+
+;; LESS
+(use-package skewer-less
+  :hook (less-css-mode . skewer-less-mode))
+
+;; JavaScript
+(use-package js2-mode
+  :mode "\\.js\\'"
+  :config
+  (add-to-list 'flycheck-disabled-checkers #'javascript-jshint)
+  (flycheck-add-mode 'javascript-eslint 'js2-mode))
+
+;; JSON mode
+(use-package json-mode
+  :mode "\\.json\\'")
+
+(use-package json-reformat
+  :after json-mode)
+
+(use-package json-snatcher
+  :after json-mode)
+
+;; Live browser JavaScript, CSS, and HTML interaction
+(use-package skewer-mode
+  :diminish
+  :hook (((js-mode js2-mode). skewer-mode)
+         (css-mode . skewer-css-mode)
+         (web-mode . skewer-html-mode)
+         (html-mode . skewer-html-mode)))
+
+(use-package typescript-mode
+  :mode ("\\.ts[x]\\'" . typescript-mode))
+
+;; Major mode for editing web templates
+(use-package web-mode
+  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+
 ;;; Programming languages support
 
 ;; C/C++ Mode
