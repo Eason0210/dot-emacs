4d48301bbb7f059c92ad047dc25dcd3886d3f007
Eason0210 <aqua0210@163.com>
2021-10-11 19:30:23 +0800

    save and restore sessions.

--- init.el
+++ init.el
@@ -1307,7 +1307,73 @@ typical word processor."
 
   (dolist (font '("Microsoft Yahei" "Hiragino Sans GB" "Noto Sans Mono CJK SC"))
     (if (font-installed-p font)
-       (set-fontset-font t '(#x4e00 . #x9fff) font))))
+        (set-fontset-font t '(#x4e00 . #x9fff) font))))
+
+
+;;; Save and restore editor sessions between restarts
+
+;; Show Emacs init time
+(defun sanityinc/time-subtract-millis (b a)
+  (* 1000.0 (float-time (time-subtract b a))))
+
+(defun sanityinc/show-init-time ()
+  (message "init completed in %.2fms"
+           (sanityinc/time-subtract-millis after-init-time before-init-time)))
+
+(add-hook 'after-init-hook 'sanityinc/show-init-time)
+
+;; Save a list of open files in ~/.emacs.d/.emacs.desktop
+(setq desktop-path (list user-emacs-directory)
+      desktop-auto-save-timeout 600)
+(desktop-save-mode 1)
+
+(defun sanityinc/desktop-time-restore (orig &rest args)
+  (let ((start-time (current-time)))
+    (prog1
+        (apply orig args)
+      (message "Desktop restored in %.2fms"
+               (sanityinc/time-subtract-millis (current-time)
+                                               start-time)))))
+(advice-add 'desktop-read :around 'sanityinc/desktop-time-restore)
+
+(defun sanityinc/desktop-time-buffer-create (orig ver filename &rest args)
+  (let ((start-time (current-time)))
+    (prog1
+        (apply orig ver filename args)
+      (message "Desktop: %.2fms to restore %s"
+               (sanityinc/time-subtract-millis (current-time)
+                                               start-time)
+               (when filename
+                 (abbreviate-file-name filename))))))
+(advice-add 'desktop-create-buffer :around 'sanityinc/desktop-time-buffer-create)
+
+
+;; Save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      '((comint-input-ring        . 50)
+        (compile-history          . 30)
+        desktop-missing-file-warning
+        (dired-regexp-history     . 20)
+        (extended-command-history . 30)
+        (face-name-history        . 20)
+        (file-name-history        . 100)
+        (grep-find-history        . 30)
+        (grep-history             . 30)
+        (magit-revision-history   . 50)
+        (minibuffer-history       . 50)
+        (org-clock-history        . 50)
+        (org-refile-history       . 50)
+        (org-tags-history         . 50)
+        (query-replace-history    . 60)
+        (read-expression-history  . 60)
+        (regexp-history           . 60)
+        (regexp-search-ring       . 20)
+        register-alist
+        (search-ring              . 20)
+        (shell-command-history    . 50)
+        tags-file-name
+        tags-table-list))
 
 
 ;;; Allow access from emacsclient
