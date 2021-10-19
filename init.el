f7816da579038aac793b98c749037f9e230f28f7
Eason Huang <aqua0210@163.com>
2021-10-19 15:35:25 +0800

    refactor the configuration of desktop

--- init.el
+++ init.el
@@ -1396,29 +1396,61 @@ typical word processor."
 (add-hook 'after-init-hook 'sanityinc/show-init-time)
 
 ;; Save a list of open files in ~/.emacs.d/.emacs.desktop
-(setq desktop-path (list user-emacs-directory)
-      desktop-auto-save-timeout 600)
-(desktop-save-mode 1)
+(use-package desktop
+  :ensure nil
+  :config
+  (setq desktop-path (list user-emacs-directory)
+        desktop-auto-save-timeout 600)
+  
+  (advice-add 'desktop-read :around 'sanityinc/desktop-time-restore)
+  (advice-add 'desktop-create-buffer :around 'sanityinc/desktop-time-buffer-create)
 
-(defun sanityinc/desktop-time-restore (orig &rest args)
-  (let ((start-time (current-time)))
-    (prog1
-        (apply orig args)
-      (message "Desktop restored in %.2fms"
-               (sanityinc/time-subtract-millis (current-time)
-                                               start-time)))))
-(advice-add 'desktop-read :around 'sanityinc/desktop-time-restore)
+  ;; Save a bunch of variables to the desktop file
+  ;; for lists specify the len of the maximal saved data also
+  (setq desktop-globals-to-save
+        '((comint-input-ring        . 50)
+          (compile-history          . 30)
+          desktop-missing-file-warning
+          (dired-regexp-history     . 20)
+          (extended-command-history . 30)
+          (face-name-history        . 20)
+          (file-name-history        . 100)
+          (grep-find-history        . 30)
+          (grep-history             . 30)
+          (magit-revision-history   . 50)
+          (minibuffer-history       . 50)
+          (org-clock-history        . 50)
+          (org-refile-history       . 50)
+          (org-tags-history         . 50)
+          (query-replace-history    . 60)
+          (read-expression-history  . 60)
+          (regexp-history           . 60)
+          (regexp-search-ring       . 20)
+          register-alist
+          (search-ring              . 20)
+          (shell-command-history    . 50)
+          tags-file-name
+          tags-table-list))
 
-(defun sanityinc/desktop-time-buffer-create (orig ver filename &rest args)
-  (let ((start-time (current-time)))
-    (prog1
-        (apply orig ver filename args)
-      (message "Desktop: %.2fms to restore %s"
-               (sanityinc/time-subtract-millis (current-time)
-                                               start-time)
-               (when filename
-                 (abbreviate-file-name filename))))))
-(advice-add 'desktop-create-buffer :around 'sanityinc/desktop-time-buffer-create)
+  (desktop-save-mode 1)
+  :preface
+  (defun sanityinc/desktop-time-restore (orig &rest args)
+    (let ((start-time (current-time)))
+      (prog1
+          (apply orig args)
+        (message "Desktop restored in %.2fms"
+                 (sanityinc/time-subtract-millis (current-time)
+                                                 start-time)))))
+  
+  (defun sanityinc/desktop-time-buffer-create (orig ver filename &rest args)
+    (let ((start-time (current-time)))
+      (prog1
+          (apply orig ver filename args)
+        (message "Desktop: %.2fms to restore %s"
+                 (sanityinc/time-subtract-millis (current-time)
+                                                 start-time)
+                 (when filename
+                   (abbreviate-file-name filename)))))))
 
 ;; Restore histories and registers after saving
 
@@ -1433,33 +1465,6 @@ typical word processor."
   (setq session-save-file (locate-user-emacs-file ".session"))
   (setq session-name-disable-regexp "\\(?:\\`'/tmp\\|\\.git/[A-Z_]+\\'\\)")
   (setq session-save-file-coding-system 'utf-8))
-
-;; Save a bunch of variables to the desktop file
-;; for lists specify the len of the maximal saved data also
-(setq desktop-globals-to-save
-      '((comint-input-ring        . 50)
-        (compile-history          . 30)
-        desktop-missing-file-warning
-        (dired-regexp-history     . 20)
-        (extended-command-history . 30)
-        (face-name-history        . 20)
-        (file-name-history        . 100)
-        (grep-find-history        . 30)
-        (grep-history             . 30)
-        (magit-revision-history   . 50)
-        (minibuffer-history       . 50)
-        (org-clock-history        . 50)
-        (org-refile-history       . 50)
-        (org-tags-history         . 50)
-        (query-replace-history    . 60)
-        (read-expression-history  . 60)
-        (regexp-history           . 60)
-        (regexp-search-ring       . 20)
-        register-alist
-        (search-ring              . 20)
-        (shell-command-history    . 50)
-        tags-file-name
-        tags-table-list))
 
 
 ;;; Allow access from emacsclient
