83a70cb1a04e047f8c558a5825626e1dce265145
Eason0210 <aqua0210@163.com>
2021-10-20 20:58:41 +0800

    should work at Emacs 27.1 and newer version

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
 (let ((minver "26.2"))
   (when (version< emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version< emacs-version "28.0.50")
+(when (version< emacs-version "27.1")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
