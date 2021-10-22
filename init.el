4cf50229c04829a940f6cc93c88a0d156ec9b387
Eason0210 <aqua0210@163.com>
2021-10-23 00:10:09 +0800

    don't install rime if rime-user-data-dir not exist

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
 ;;; Code:
 
 ;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
-;; (setq debug-on-error t)
+(setq debug-on-error t)
 
 (let ((minver "26.2"))
   (when (version< emacs-version minver)
@@ -1371,50 +1371,48 @@ there is no current file, eval the current buffer."
   (super-save-mode 1))
 
 ;; Rime
+(defconst rime-usr-data-exists-p
+  (file-exists-p "~/emacs-data/rime")
+  "Checking if there is a rime user data.")
 
-(use-package rime
-  :if rime-usr-data-exists-p
-  :bind (("C-\\" . toggle-input-method)
-         ("C-`" . rime-send-keybinding)
-         ([f8] . rime-toggle-show-candidate))
-  :init
-  (setq
-   rime-inline-predicates '(rime-predicate-space-after-cc-p
-                            rime-predicate-current-uppercase-letter-p)
-   rime-translate-keybindings '("C-f" "C-b" "C-n" "C-p" "C-g")
-   rime-inline-ascii-trigger 'shift-r
-   rime-inline-ascii-holder ?a
-   default-input-method "rime"
-   rime-cursor "|"
-   rime-show-candidate nil
-   window-min-height 1
-   rime-user-data-dir "~/emacs-data/rime"
-   rime-title "")
-  (when (eq system-type 'windows-nt)
-    (setq rime-share-data-dir
-          "~/scoop/apps/msys2/current/mingw64/share/rime-data"))
-  (when *is-a-mac*
-    (setq rime-librime-root  "~/emacs-data/librime/dist")
-    (setq rime-emacs-module-header-root "~/.nix-profile/include"))
-  :config
-  ;; change cursor color automatically
-  (use-package im-cursor-chg
-    :ensure nil
-    :after rime
+(when rime-usr-data-exists-p
+  (use-package rime
+    :bind (("C-\\" . toggle-input-method)
+           ("C-`" . rime-send-keybinding)
+           ([f8] . rime-toggle-show-candidate))
+    :init
+    (setq
+     rime-inline-predicates '(rime-predicate-space-after-cc-p
+                              rime-predicate-current-uppercase-letter-p)
+     rime-translate-keybindings '("C-f" "C-b" "C-n" "C-p" "C-g")
+     rime-inline-ascii-trigger 'shift-r
+     rime-inline-ascii-holder ?a
+     default-input-method "rime"
+     rime-cursor "|"
+     rime-show-candidate nil
+     window-min-height 1
+     rime-user-data-dir "~/emacs-data/rime"
+     rime-title "")
+    (when (eq system-type 'windows-nt)
+      (setq rime-share-data-dir
+            "~/scoop/apps/msys2/current/mingw64/share/rime-data"))
+    (when *is-a-mac*
+      (setq rime-librime-root  "~/emacs-data/librime/dist")
+      (setq rime-emacs-module-header-root "~/.nix-profile/include"))
     :config
-    (cursor-chg-mode 1))
-  :preface
-  (defconst rime-usr-data-exists-p
-    (file-exists-p "~/emacs-data/rime")
-    "Checking if there is a rime user data.")
-
-  (defun rime-toggle-show-candidate ()
-    "Use minibuffer for candidate if current is nil."
-    (interactive)
-    (if (equal rime-show-candidate nil)
-        (setq rime-show-candidate 'minibuffer)
-      (setq rime-show-candidate nil))))
-
+    ;; change cursor color automatically
+    (use-package im-cursor-chg
+      :ensure nil
+      :after rime
+      :config
+      (cursor-chg-mode 1))
+    :preface  
+    (defun rime-toggle-show-candidate ()
+      "Use minibuffer for candidate if current is nil."
+      (interactive)
+      (if (equal rime-show-candidate nil)
+          (setq rime-show-candidate 'minibuffer)
+        (setq rime-show-candidate nil)))))
 
 
 ;;; Dictionaries
