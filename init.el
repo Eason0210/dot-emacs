6e7963459a266f662a22dedf4f3bbd187f24c138
Eason0210 <aqua0210@163.com>
2021-10-23 22:16:33 +0800

    configure isearch to get better experience

--- init.el
+++ init.el
@@ -280,6 +280,47 @@
   :config
   (diredfl-global-mode 1))
 
+;;; Isearch settings
+
+;; Show number of matches while searching
+(use-package anzu
+  :hook (after-init . global-anzu-mode)
+  :bind (([remap query-replace-regexp] . anzu-query-replace-regexp)
+         ([remap query-replace] . anzu-query-replace-regexp))
+  :config
+  (setq anzu-mode-lighter ""))
+
+(use-package isearch
+  :ensure nil
+  ;; DEL during isearch should edit the search string, not jump back to the previous result
+  :bind (:map isearch-mode-map
+              ([remap isearch-delete-char] . isearch-del-char)
+              ("C-c C-o" . isearch-occur)
+              ("\C-\M-w" . isearch-yank-symbol)
+              ([(control return)] . sanityinc/isearch-exit-other-end))
+  :preface
+  ;; Search back/forth for the symbol at point
+  ;; See http://www.emacswiki.org/emacs/SearchAtPoint
+  (defun isearch-yank-symbol ()
+    "*Put symbol at current point into search string."
+    (interactive)
+    (let ((sym (thing-at-point 'symbol)))
+      (if sym
+          (progn
+            (setq isearch-regexp t
+                  isearch-string (concat "\\_<" (regexp-quote sym) "\\_>")
+                  isearch-message (mapconcat 'isearch-text-char-description isearch-string "")
+                  isearch-yank-flag t))
+        (ding)))
+    (isearch-search-and-update))
+
+  (defun sanityinc/isearch-exit-other-end ()
+    "Exit isearch, but at the other end of the search string.
+This is useful when followed by an immediate kill."
+    (interactive)
+    (isearch-exit)
+    (goto-char isearch-other-end)))
+
 
 ;;; Ibuffer settings
 
