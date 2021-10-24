c57db7e53aff4861398079cc4d506b96456c68dd
Eason0210 <aqua0210@163.com>
2021-10-24 14:24:32 +0800

    Config capturing, refiling, to-do things, org-agenda and org-clock

--- init.el
+++ init.el
@@ -1013,6 +1013,210 @@ Call a second time to restore the original window configuration."
         org-export-kill-product-buffer-when-displayed t
         org-tags-column 80
         org-hide-emphasis-markers t)
+  ;; Re-align tags when window shape changes
+  (with-eval-after-load 'org-agenda
+    (add-hook 'org-agenda-mode-hook
+              (lambda ()
+                (add-hook
+                 'window-configuration-change-hook 'org-agenda-align-tags nil t))))
+
+  ;; Capturing
+  (setq org-directory "~/org/agenda/")
+  (setq org-default-notes-file (concat org-directory "inbox.org"))
+
+  (setq org-capture-templates
+        `(("t" "todo" entry (file "") ; "" => `org-default-notes-file'
+           "* NEXT %?\n%U\n" :clock-resume t)
+          ("n" "note" entry (file "")
+           "* %? :NOTE:\n%U\n%a\n" :clock-resume t)
+          ))
+
+  
+  ;; Refiling
+
+  (setq org-refile-use-cache nil)
+
+  ;; Targets include this file and any file contributing to the agenda - up to 5 levels deep
+  (setq org-refile-targets '((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5)))
+
+  (with-eval-after-load 'org-agenda
+    (add-to-list 'org-agenda-after-show-hook 'org-show-entry))
+
+  (advice-add 'org-refile :after (lambda (&rest _) (org-save-all-org-buffers)))
+
+  ;; Exclude DONE state tasks from refile targets
+  (defun sanityinc/verify-refile-target ()
+    "Exclude todo keywords with a done state from refile targets."
+    (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+  (setq org-refile-target-verify-function 'sanityinc/verify-refile-target)
+
+  (defun sanityinc/org-refile-anywhere (&optional goto default-buffer rfloc msg)
+    "A version of `org-refile' which allows refiling to any subtree."
+    (interactive "P")
+    (let ((org-refile-target-verify-function))
+      (org-refile goto default-buffer rfloc msg)))
+
+  (defun sanityinc/org-agenda-refile-anywhere (&optional goto rfloc no-update)
+    "A version of `org-agenda-refile' which allows refiling to any subtree."
+    (interactive "P")
+    (let ((org-refile-target-verify-function))
+      (org-agenda-refile goto rfloc no-update)))
+
+  ;; Targets start with the file name - allows creating level 1 tasks
+  ;;(setq org-refile-use-outline-path (quote file))
+  (setq org-refile-use-outline-path t)
+  (setq org-outline-path-complete-in-steps nil)
+
+  ;; Allow refile to create parent tasks with confirmation
+  (setq org-refile-allow-creating-parent-nodes 'confirm)
+
+
+  ;; To-do settings
+  (setq org-todo-keywords
+        (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+                (sequence "PROJECT(p)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
+                (sequence "WAITING(w@/!)" "DELEGATED(e!)" "HOLD(h)" "|" "CANCELLED(c@/!)")))
+        org-todo-repeat-to-state "NEXT")
+
+  (setq org-todo-keyword-faces
+        (quote (("NEXT" :inherit warning)
+                ("PROJECT" :inherit font-lock-string-face))))
+
+
+  ;; Agenda views
+
+  (setq-default org-agenda-clockreport-parameter-plist '(:link t :maxlevel 3))
+
+
+  (let ((active-project-match "-INBOX/PROJECT"))
+
+    (setq org-stuck-projects
+          `(,active-project-match ("NEXT")))
+
+    (setq org-agenda-compact-blocks t
+          org-agenda-sticky t
+          org-agenda-start-on-weekday nil
+          org-agenda-span 'day
+          org-agenda-include-diary nil
+          org-agenda-sorting-strategy
+          '((agenda habit-down time-up user-defined-up effort-up category-keep)
+            (todo category-up effort-up)
+            (tags category-up effort-up)
+            (search category-up))
+          org-agenda-window-setup 'current-window
+          org-agenda-custom-commands
+          `(("N" "Notes" tags "NOTE"
+             ((org-agenda-overriding-header "Notes")
+              (org-tags-match-list-sublevels t)))
+            ("g" "GTD"
+             ((agenda "" nil)
+              (tags "INBOX"
+                    ((org-agenda-overriding-header "Inbox")
+                     (org-tags-match-list-sublevels nil)))
+              (stuck ""
+                     ((org-agenda-overriding-header "Stuck Projects")
+                      (org-agenda-tags-todo-honor-ignore-options t)
+                      (org-tags-match-list-sublevels t)
+                      (org-agenda-todo-ignore-scheduled 'future)))
+              (tags-todo "-INBOX"
+                         ((org-agenda-overriding-header "Next Actions")
+                          (org-agenda-tags-todo-honor-ignore-options t)
+                          (org-agenda-todo-ignore-scheduled 'future)
+                          (org-agenda-skip-function
+                           '(lambda ()
+                              (or (org-agenda-skip-subtree-if 'todo '("HOLD" "WAITING"))
+                                  (org-agenda-skip-entry-if 'nottodo '("NEXT")))))
+                          (org-tags-match-list-sublevels t)
+                          (org-agenda-sorting-strategy
+                           '(todo-state-down effort-up category-keep))))
+              (tags-todo ,active-project-match
+                         ((org-agenda-overriding-header "Projects")
+                          (org-tags-match-list-sublevels t)
+                          (org-agenda-sorting-strategy
+                           '(category-keep))))
+              (tags-todo "-INBOX/-NEXT"
+                         ((org-agenda-overriding-header "Orphaned Tasks")
+                          (org-agenda-tags-todo-honor-ignore-options t)
+                          (org-agenda-todo-ignore-scheduled 'future)
+                          (org-agenda-skip-function
+                           '(lambda ()
+                              (or (org-agenda-skip-subtree-if 'todo '("PROJECT" "HOLD" "WAITING" "DELEGATED"))
+                                  (org-agenda-skip-subtree-if 'nottododo '("TODO")))))
+                          (org-tags-match-list-sublevels t)
+                          (org-agenda-sorting-strategy
+                           '(category-keep))))
+              (tags-todo "/WAITING"
+                         ((org-agenda-overriding-header "Waiting")
+                          (org-agenda-tags-todo-honor-ignore-options t)
+                          (org-agenda-todo-ignore-scheduled 'future)
+                          (org-agenda-sorting-strategy
+                           '(category-keep))))
+              (tags-todo "/DELEGATED"
+                         ((org-agenda-overriding-header "Delegated")
+                          (org-agenda-tags-todo-honor-ignore-options t)
+                          (org-agenda-todo-ignore-scheduled 'future)
+                          (org-agenda-sorting-strategy
+                           '(category-keep))))
+              (tags-todo "-INBOX"
+                         ((org-agenda-overriding-header "On Hold")
+                          (org-agenda-skip-function
+                           '(lambda ()
+                              (or (org-agenda-skip-subtree-if 'todo '("WAITING"))
+                                  (org-agenda-skip-entry-if 'nottodo '("HOLD")))))
+                          (org-tags-match-list-sublevels nil)
+                          (org-agenda-sorting-strategy
+                           '(category-keep))))
+              ;; (tags-todo "-NEXT"
+              ;;            ((org-agenda-overriding-header "All other TODOs")
+              ;;             (org-match-list-sublevels t)))
+              )))))
+
+
+  (add-hook 'org-agenda-mode-hook 'hl-line-mode)
+
+
+  ;; Org clock
+
+  ;; Save the running clock and all clock history when exiting Emacs, load it on startup
+  (with-eval-after-load 'org
+    (org-clock-persistence-insinuate))
+  (setq org-clock-persist t)
+  (setq org-clock-in-resume t)
+
+  ;; Save clock data and notes in the LOGBOOK drawer
+  (setq org-clock-into-drawer t)
+  ;; Save state changes in the LOGBOOK drawer
+  (setq org-log-into-drawer t)
+  ;; Removes clocked tasks with 0:00 duration
+  (setq org-clock-out-remove-zero-time-clocks t)
+
+  ;; Show clock sums as hours and minutes, not "n days" etc.
+  (setq org-time-clocksum-format
+        '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+
+
+  ;; Show the clocked-in task - if any - in the header line
+  (defun sanityinc/show-org-clock-in-header-line ()
+    (setq-default header-line-format '((" " org-mode-line-string " "))))
+
+  (defun sanityinc/hide-org-clock-from-header-line ()
+    (setq-default header-line-format nil))
+
+  (add-hook 'org-clock-in-hook 'sanityinc/show-org-clock-in-header-line)
+  (add-hook 'org-clock-out-hook 'sanityinc/hide-org-clock-from-header-line)
+  (add-hook 'org-clock-cancel-hook 'sanityinc/hide-org-clock-from-header-line)
+
+  (with-eval-after-load 'org-clock
+    (define-key org-clock-mode-line-map [header-line mouse-2] 'org-clock-goto)
+    (define-key org-clock-mode-line-map [header-line mouse-1] 'org-clock-menu))
+
+
+  ;; Archiving
+
+  (setq org-archive-mark-done nil)
+  (setq org-archive-location "%s_archive::* Archive")
+
+  
   ;; Babel
   (setq org-confirm-babel-evaluate nil
         org-src-fontify-natively t
