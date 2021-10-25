a46e86ca3236e13d07fe465479b3fcab208fb18e
Eason0210 <aqua0210@163.com>
2021-10-25 21:35:17 +0800

    Make company works with yasnippet, and add more key bindings

--- init.el
+++ init.el
@@ -427,13 +427,30 @@ This is useful when followed by an immediate kill."
 
 
 ;; Completion with company
+;; WAITING: haskell-mode sets tags-table-list globally, breaks tags-completion-at-point-function
+;; TODO: Default sort order should place [a-z] before punctuation
+
 (use-package company
   :diminish
-  :bind
-  (:map company-active-map
-        ("TAB" . smarter-tab-to-complete)
-        ("<tab>" . smarter-tab-to-complete))
   :hook (after-init . global-company-mode)
+  :bind (("M-C-/" . company-complete)
+         :map company-mode-map
+         ("M-/" . company-complete)
+         ([remap completion-at-point] . company-complete)
+         ([remap indent-for-tab-command] . company-indent-or-complete-common)
+         :map company-active-map
+         ("M-/" . company-other-backend)
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous)
+         ("C-d" . company-show-doc-buffer)
+         ("M-." . company-show-location)
+         ("<tab>" . smarter-tab-to-complete))
+  :init
+  (setq tab-always-indent 'complete)
+  (add-to-list 'completion-styles 'initials t)
+  :config
+  (setq-default company-dabbrev-other-buffers 'all
+                company-tooltip-align-annotations t)
   :preface
   (defun smarter-tab-to-complete ()
     "Try to `org-cycle', `yas-expand', and `yas-next-field' at current
