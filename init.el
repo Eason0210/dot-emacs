7c757c71e2fa565d9ab03f5025184982f0ab0146
Eason0210 <aqua0210@163.com>
2021-10-30 23:50:49 +0800

    Add comments on how to use GTD in org-mode

--- init.el
+++ init.el
@@ -1071,6 +1071,25 @@ Call a second time to restore the original window configuration."
 
 ;;; Org-mode config
 
+;; Among settings for many aspects of `org-mode', this code includes
+;; an opinionated setup for the Getting Things Done (GTD) system based
+;; around the Org Agenda.  I have an "inbox.org" file with a header
+;; including
+
+;;     #+CATEGORY: Inbox
+;;     #+FILETAGS: INBOX
+
+;; and then set this file as `org-default-notes-file'.  Captured org
+;; items will then go into this file with the file-level tag, and can
+;; be refiled to other locations as necessary.
+
+;; Those other locations are generally other org files, which should
+;; be added to `org-agenda-files-list' (along with "inbox.org" org).
+;; With that done, there's then an agenda view, accessible via the
+;; `org-agenda' command, which gives a convenient overview.
+;; `org-todo-keywords' is customised here to provide corresponding
+;; TODO states, which should make sense to GTD adherents.
+
 (use-package org
   :ensure nil
   :bind (("C-c a" . org-agenda)
@@ -1101,6 +1120,10 @@ Call a second time to restore the original window configuration."
         org-export-kill-product-buffer-when-displayed t
         org-tags-column 80
         org-hide-emphasis-markers t)
+
+
+  ;; Lots of stuff from http://doc.norang.ca/org-mode.html
+
   ;; Re-align tags when window shape changes
   (with-eval-after-load 'org-agenda
     (add-hook 'org-agenda-mode-hook
