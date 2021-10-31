47fbd2d7fa911587a4052db6149ed529afc43b6a
Eason0210 <aqua0210@163.com>
2021-10-31 22:23:11 +0800

    Add ibuffer-vc to enhance Ibuffer and ajust the ibuffer-formats

--- init.el
+++ init.el
@@ -346,7 +346,51 @@ This is useful when followed by an immediate kill."
 
 (use-package ibuffer
   :ensure nil
-  :bind ("C-x C-b" . ibuffer))
+  :bind ("C-x C-b" . ibuffer)
+  :config
+  (setq-default ibuffer-show-empty-filter-groups nil)
+
+  ;; Use human readable Size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (file-size-human-readable (buffer-size)))
+
+  ;; Modify the default ibuffer-formats (toggle with `)
+  (setq ibuffer-formats
+        '((mark modified read-only vc-status-mini " "
+                (name 22 22 :left :elide)
+                " "
+                (size-h 9 -1 :right)
+                " "
+                (mode 12 12 :left :elide)
+                " "
+                vc-relative-file)
+          (mark modified read-only vc-status-mini " "
+                (name 22 22 :left :elide)
+                " "
+                (size-h 9 -1 :right)
+                " "
+                (mode 14 14 :left :elide)
+                " "
+                (vc-status 12 12 :left)
+                " "
+                vc-relative-file)))
+
+  (setq ibuffer-filter-group-name-face 'font-lock-doc-face))
+
+(use-package fullframe
+  :after ibuffer
+  :config
+  (fullframe ibuffer ibuffer-quit))
+
+(use-package ibuffer-vc
+  :after ibuffer
+  :config
+  (defun ibuffer-set-up-preferred-filters ()
+    (ibuffer-vc-set-filter-groups-by-vc-root)
+    (unless (eq ibuffer-sorting-mode 'filename/process)
+      (ibuffer-do-sort-by-filename/process)))
+  (add-hook 'ibuffer-hook 'ibuffer-set-up-preferred-filters))
 
 
 ;;; Configure FlyCheck global behavior
