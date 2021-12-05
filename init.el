1085301a89f9c624fbe4416e7e3514a923ebcfcd
Eason0210 <aqua0210@163.com>
2021-12-05 14:25:23 +0800

    Remove emacs-rime, osx-dictionary and fanyi

--- init.el
+++ init.el
@@ -2163,69 +2163,8 @@ there is no current file, eval the current buffer."
   (sis-global-context-mode t)
   (sis-global-inline-mode t))
 
-;;; Emacs-rime input method
-
-(defconst rime-usr-data-exists-p
-  (file-exists-p "~/emacs-data/rime")
-  "Checking if there is a rime user data.")
-
-(when rime-usr-data-exists-p
-  (use-package rime
-    :bind (("C-\\" . toggle-input-method)
-           ("C-`" . rime-send-keybinding)
-           ([f8] . rime-toggle-show-candidate))
-    :init
-    (setq
-     rime-inline-predicates '(rime-predicate-space-after-cc-p
-                              rime-predicate-current-uppercase-letter-p)
-     rime-translate-keybindings '("C-f" "C-b" "C-n" "C-p" "C-g")
-     rime-inline-ascii-trigger 'shift-r
-     rime-inline-ascii-holder ?a
-     default-input-method "rime"
-     rime-cursor "|"
-     rime-show-candidate nil
-     window-min-height 1
-     rime-user-data-dir "~/emacs-data/rime"
-     rime-title "")
-    (when (eq system-type 'windows-nt)
-      (setq rime-share-data-dir
-            "~/scoop/apps/msys2/current/mingw64/share/rime-data"))
-    (when *is-a-mac*
-      (setq rime-librime-root  "~/emacs-data/librime/dist")
-      (setq rime-emacs-module-header-root "~/.nix-profile/include"))
-    :config
-    ;; change cursor color automatically
-    (use-package im-cursor-chg
-      :ensure nil
-      :after rime
-      :config
-      (cursor-chg-mode 1))
-    :preface
-    (defun rime-toggle-show-candidate ()
-      "Use minibuffer for candidate if current is nil."
-      (interactive)
-      (if (equal rime-show-candidate nil)
-          (setq rime-show-candidate 'minibuffer)
-        (setq rime-show-candidate nil)))))
-
 
 ;;; Dictionaries
-
-(when *is-a-mac*
-  (use-package osx-dictionary
-    :bind (("C-c t i" . osx-dictionary-search-input)
-           ("C-c t x" . osx-dictionary-search-pointer))))
-
-(use-package fanyi
-  :bind (("C-c t f" . fanyi-dwim)
-         ("C-c t d" . fanyi-dwim2))
-  :config
-  (setq fanyi-haici-chart-inhibit-same-window t)
-  :custom
-  (fanyi-providers '(fanyi-haici-provider
-                     fanyi-youdao-thesaurus-provider
-                     fanyi-etymon-provider
-                     fanyi-longman-provider)))
 
 (use-package go-translate
   :bind (("C-c t g" . gts-do-translate)
