6cc1875379ce67e8d7d65cf95422b6af9d3e3f23
Eason0210 <aqua0210@163.com>
2022-02-26 21:30:12 +0800

    Add exec-path-from-shell

--- init.el
+++ init.el
@@ -116,6 +116,19 @@
   (defalias 'upgrade-packages #'auto-package-update-now))
 
 
+;; Set up exec-path to help Emacs find programs
+(use-package exec-path-from-shell
+  :init
+  (with-eval-after-load 'exec-path-from-shell
+    (dolist (var '("GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+      (add-to-list 'exec-path-from-shell-variables var)))
+
+  (when (or (memq window-system '(mac ns x))
+            (unless (memq system-type '(ms-dos windows-nt))
+              (daemonp)))
+    (exec-path-from-shell-initialize)))
+
+
 (use-package diminish)
 
 (use-package bind-key
