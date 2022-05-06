18ceebd0ff43cf896431e67093cfc47901658c43
Eason0210 <aqua0210@163.com>
2022-05-06 19:22:58 +0800

    Adjust font settings

--- early-init.el
+++ early-init.el
@@ -12,6 +12,20 @@
 (setq org-modules-loaded t)
 (setq frame-inhibit-implied-resize t)
 
+;; Set default font before frame creation to make sure the first frame have the correct size
+(defvar font-list
+  (cond
+   ((eq system-type 'darwin)
+    '(("SF Mono" . 13) ("Monaco" . 13) ("Menlo" . 13)))
+   ((eq system-type 'windows-nt)
+    '(("SF Mono" . 11) ("Consolas" . 12) ("Cascadia Mono" . 11)))
+   (t
+    '(("SF Mono" . 11) ("Consolas" . 12) ("Cascadia Mono" . 11))))
+  "List of fonts and sizes.  The first one available will be used.")
+
+(add-to-list 'default-frame-alist (cons 'font (format "%s-%d" (caar font-list) (cdar font-list))))
+
+
 ;; So we can detect this having been loaded
 (provide 'early-init)
 
