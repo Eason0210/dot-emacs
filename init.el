369d0d390af7c45c09c81fa39512d03eab4fd768
Eason0210 <aqua0210@163.com>
2022-05-06 19:29:28 +0800

    org-roam: Tweat key binding and use sqlite-builtin

--- init.el
+++ init.el
@@ -1474,28 +1474,29 @@ typical word processor."
       (when (fboundp 'writeroom-mode)
         (writeroom-mode 0)))))
 
-
 ;; Roam
 (when (and (executable-find "sqlite3") (executable-find "cc"))
+  (use-package emacsql-sqlite-builtin :defer t)
   (use-package org-roam
     :diminish
-    :bind (("C-c n l" . org-roam-buffer-toggle)
+    :bind (("C-c n a" . org-id-get-create)
+           ("C-c n l" . org-roam-buffer-toggle)
            ("C-c n f" . org-roam-node-find)
            ("C-c n g" . org-roam-graph)
            ("C-c n i" . org-roam-node-insert)
            ("C-c n c" . org-roam-capture)
            ("C-c n j" . org-roam-dailies-capture-today)
            ("C-c n r" . org-roam-ref-find)
+           ("C-c n R" . org-roam-ref-add)
            ("C-c n s" . org-roam-db-sync))
-    :init
-    (setq org-roam-directory (file-truename "~/.org/org-roam")
-          org-roam-db-location "~/.org/org-roam.db"
-          org-roam-db-gc-threshold most-positive-fixnum
-          org-roam-v2-ack t)
-
+    :custom
+    (org-roam-database-connector 'sqlite-builtin)
+    (org-roam-directory (file-truename "~/.org/org-roam"))
+    (org-roam-db-location "~/.org/org-roam.db")
+    (org-roam-db-gc-threshold most-positive-fixnum)
+    :config
     (unless (file-exists-p org-roam-directory)
       (make-directory org-roam-directory t))
-    :config
     (org-roam-db-autosync-enable)
     (add-to-list 'display-buffer-alist
                  '("\\*org-roam\\*"
